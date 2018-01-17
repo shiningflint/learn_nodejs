59b5084c9317175a4adc0e636ee2fbccc07ec61c
Adam Christopher <adam.keristoper@gmail.com>
2018-01-17 18:01:06 +0900

    added express using ejs viewing template

---
+++ ex_server.js
@@ -0,0 +1,17 @@
+const ejs = require('ejs')
+const express = require('express')
+const app = express()
+const port = 3006
+
+app.set('view engine', 'html')
+app.engine('html', ejs.renderFile)
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/kentang', (req, res) => {
+  res.render('potato')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
